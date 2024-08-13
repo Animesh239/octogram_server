44ae8291e451e3145c8d9c5a945966b7a1676e6f
Animesh239 <animeshbarik239@gmail.com>
2024-08-13 19:54:19 +0530

    rds

--- config/db.js
+++ config/db.js
@@ -1,10 +1,10 @@
 const mysql2 = require('mysql2/promise');
 
 const mysqlpool = mysql2.createPool({
-    host: 'ec2-13-234-122-156.ap-south-1.compute.amazonaws.com',
+    host: 'database-1.cz42ou6yyfqe.ap-south-1.rds.amazonaws.com',
     user: 'root',
     password: 'Animesh@1311',
-    database: 'problems_db',
+    database: 'mysql',
     waitForConnections: true,
 });
 
